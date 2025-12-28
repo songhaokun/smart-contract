6f3209e790169a0cb65c9e1fdb78e0e7a7c3d75c
HemaDeviU <uhemadevi1927@gmail.com>
2025-12-28 10:31:28 +0530

    contract setup

---
+++ src/MneeMart.sol
@@ -0,0 +1,10 @@
+//SPDX License Identifier:MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract MneeMart is Ownable, ReentrancyGuard 
+    {
+        
+    }
